747a59c3e395f2cc541857417a63d4d48ba217f1
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-12-23 16:44:01 -0500

    feat: add asyncHandler and error handling to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const db = require("../db/queries");
 const bcrypt = require("bcryptjs");
 const asyncHandler = require('express-async-handler');
-
+const CustomError = require("../helper/CustomError");
 
 function getLoginPage(req, res, next) {
   res.send('Login Page');
@@ -11,33 +11,20 @@ function getSignUpPage(req, res, next) {
   res.send('Sign Up Page');
 }
 
-async function createUser(req, res, next) {
+const createUser = asyncHandler(async (req, res, next) => {
   const {username, email, password} = req.body;
-  
-  try {  
-    const hash = await bcrypt.hash(password, 10)
-    const user = await db.createUser(username, email, hash);
-    if(!user) {
-      res.status(401).json({
-        success: false,
-        message: 'Failed to create user'
-      });
-    } else {
-      res.status(200).json({
-        success: true,
-        message: 'Created user'
-      });
-    }
-  } catch (error) {
-    console.log(error)
+
+  const hash = await bcrypt.hash(password, 10)
+  const user = await db.createUser(username, email, hash);
+  if(!user) {
+    next(new CustomError('Create error', 'Failed to create user', 404))
+  } else {
+    res.status(200).json({
+      success: true,
+      message: 'Created user'
+    });
   }
-}
-
-
-// async function something(asyncHandler(params)) {
-  
-// }
-
+})
 
 module.exports = {
   getLoginPage,
